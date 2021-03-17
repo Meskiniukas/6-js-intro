0921aa0e30d39ae45a1d00237407120d779f3869
Tomas <tomas.v.mob@gmail.com>
2021-03-17 16:56:17 +0200

    CW #1 +

--- js/CW-01-tryout.js
+++ js/CW-01-tryout.js
@@ -2,12 +2,12 @@ function positiveSum(arr) {
     let ps = 0;
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] >= 0) {
-            ps = ps + arr[i]      
+            ps += arr[i]
         }
     }
       console.log(ps);
       return ps
   }
 
-    console.log(positiveSum([1, 2, 4, 4, 5]));
+    console.log(positiveSum([1, 2, 4, 4, 5, -6]));
 console.log('Labas rytas');
